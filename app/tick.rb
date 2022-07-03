d7829882b6cdaad4dfedfd6774f37ebd5ec8be80
Matt Ruzicka <mattruzicka@gmail.com>
2022-07-03 14:08:01 -0500

    Show frames per second when under 55 FPS for more than 10 consecutive frames

--- app/tick.rb
+++ app/tick.rb
@@ -5,6 +5,7 @@ def boot(args)
   world = World.new
   args.state.world = world
   args.outputs.static_sprites << world.cells
+  @slow_fps_count = 0
 end
 
 def tick(args)
@@ -13,6 +14,22 @@ def tick(args)
     world.reset
   elsif args.state.tick_count.positive?
     world.compute_cells
+  end
+
+  if args.gtk.current_framerate < 55 && args.state.tick_count > 100
+    if (@slow_fps_count += 1) > 10
+      fps = args.gtk.current_framerate.round
+      args.outputs.labels << { x: 1274,
+                               y: 19,
+                               r: 255 - (fps * 2),
+                               g: 77,
+                               b: 77,
+                               alignment_enum: 2,
+                               size_enum: -3,
+                               text: "#{fps} FPS" }
+    end
+  else
+    @slow_fps_count = 0
   end
 
   # args.outputs.debug << args.gtk.framerate_diagnostics_primitives
